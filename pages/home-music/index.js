dad1f646523ab40fce265a6f240a7e1728268db0
lisongyu <lisongyu@bailian.ai>
2022-03-06 19:26:01 +0800

    课程4完成

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -2,6 +2,7 @@
 import { getBanners } from '../../service/api_music'
 import queryRect from '../../utils/query-rect'
 import throttle from '../../utils/throttle'
+import { rankingStore } from '../../store/index'
 
 const throttleQueryRect = throttle(queryRect, 1000)
 
@@ -14,6 +15,17 @@ Page({
   onLoad: function (options) {
     // 获取页面数据
     this.getPageData()
+
+
+      // 发起共享数据的请求
+      rankingStore.dispatch("getRankingDataAction")
+
+      // 从store获取共享的数据
+      rankingStore.onState("hotRanking", (res) => {
+        if (!res.tracks) return
+        const recommendSongs = res.tracks.slice(0, 6)
+        this.setData({ recommendSongs })
+      })
   },
 
   // 网络请求
